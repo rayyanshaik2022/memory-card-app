4a66c6741f20187d532467187fe9fd368597150c
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2023-08-01 16:50:57 -0400

    Score and reset game now work properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import {
-    Box,
-    Flex,
-    Heading,
-    Grid,
-    Spacer,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalCloseButton,
-    ModalBody,
-    ModalFooter,
-    Button,
-    useDisclosure,
-    VStack,
+  Box,
+  Flex,
+  Heading,
+  Grid,
+  Spacer,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  VStack,
 } from "@chakra-ui/react";
 
 import pixelArtImage from "./assets/pixel_art_bg.png";
@@ -21,161 +21,153 @@ import { useEffect, useState } from "react";
 
 import PokeCard from "./components/PokeCard";
 
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-    const [score, setScore] = useState(0);
-    const [best, setBest] = useState(0);
-    const [badChoice, setBadChoice] = useState(false);
-    const [anyCardClicked, setAnyCardClicked] = useState(0);
+  const [score, setScore] = useState(0);
+  const [best, setBest] = useState(0);
+  const [finalScore, setFinalScore] = useState(false);
+  const [anyCardClicked, setAnyCardClicked] = useState(0);
 
-    function createSeen() {
-        let newObj = {};
-        for (let i = 1; i < 152; i++) {
-            newObj[i] = false;
-        }
-
-        return newObj;
-    }
-    const [seen, setSeen] = useState(createSeen);
-
-    const [cards, setCards] = useState([]);
-
-    function updateBestScore() {
-        if (score > best) {
-            setBest(score);
-        }
+  function createSeen() {
+    let newObj = {};
+    for (let i = 1; i < 152; i++) {
+      newObj[i] = false;
     }
 
-    let cardClick = (index) => () => {
-        if (seen[index] == true) {
-            setBadChoice(true);
-            onOpen()
-        } else {
-            seen[index] = true;
-            setScore(score + 1);
-            setAnyCardClicked(anyCardClicked + 1);
-            return;
-        }
-    };
+    return newObj;
+  }
+  const [seen, setSeen] = useState(createSeen);
 
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+  const [cards, setCards] = useState([]);
+
+  let cardClick = (index) => () => {
+    if (seen[index] == true) {
+      onOpen();
+      setScore(0);
+      setFinalScore(score);
+    } else {
+      seen[index] = true;
+      setScore(score + 1);
+      setAnyCardClicked(anyCardClicked + 1);
+    }
+  };
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function createCards(n) {
+    let cards = [];
+    for (let i = 0; i < n; i++) {
+      cards.push({
+        index: getRandomInt(1, 151),
+        handleClick: cardClick,
+        id: uuidv4(),
+      });
     }
 
-    function createCards(n) {
-        let cards = [];
-        for (let i = 0; i < n; i++) {
-            cards.push({
-                index: getRandomInt(1, 151),
-                handleClick: cardClick,
-                id: uuidv4()
-            });
-        }
+    setCards(cards);
+  }
 
-        setCards(cards);
-        
+  function resetGame() {
+    createCards(8);
+    setSeen(createSeen());
+    onClose();
+  }
+
+
+  useEffect(() => {
+    createCards(8);
+    return () => {};
+  }, [anyCardClicked]);
+
+  useEffect(() => {
+    if (score > best) {
+      setBest(score);
     }
+    return () => {};
+  }, [score]);
 
-    useEffect(() => {
-        console.log("effect");
-        return () => {
-            console.log("cean");
-        };
-    }, [badChoice]);
-
-    useEffect(() => {
-        createCards(8);
-        console.log(cards)
-        return () => {};
-    }, [anyCardClicked]);
-
-    useEffect(() => {
-      if (score > best) {
-        setBest(score);
-      }
-      return () => {};
-    }, [score])
-
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    return (
-        <Box
-            backgroundImage={pixelArtImage}
-            backgroundPosition={"center"}
-            bgSize={"cover"}
-            width={"100vw"}
-            height={"100vh"}
-            padding={12}
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <Box
+      backgroundImage={pixelArtImage}
+      backgroundPosition={"center"}
+      bgSize={"cover"}
+      width={"100vw"}
+      height={"100vh"}
+      padding={12}
+    >
+      <Flex flexDir="column" align={"center"} gap={32}>
+        <Flex
+          justifyContent={"center"}
+          width={"100%"}
+          alignContent={"center"}
+          color={"gray.700"}
+          wrap={"wrap"}
         >
-            <Flex flexDir="column" align={"center"} gap={32}>
-                <Flex
-                    justifyContent={"center"}
-                    width={"100%"}
-                    alignContent={"center"}
-                    color={"gray.700"}
-                    wrap={"wrap"}
-                >
-                    <Heading as={"h1"} size={"2xl"} whiteSpace={"nowrap"}>
-                        {" "}
-                        Pokemon Memory Game{" "}
-                    </Heading>
-                    <Spacer />
-                    <Flex gap={16}>
-                        <Heading>Score: {score}</Heading>
-                        <Heading>Best: {best}</Heading>
-                    </Flex>
-                </Flex>
-                <Grid
-                    templateColumns={{
-                        base: "1fr",
-                        sm: "repeat(2, 1fr)",
-                        xl: "repeat(4, 1fr)",
-                    }}
-                    gap={{ base: 6, lg: 16 }}
-                >
-                    {/* <PokeCard index={"3"} handleClick={cardClick} />
+          <Heading as={"h1"} size={"2xl"} whiteSpace={"nowrap"}>
+            {" "}
+            Pokemon Memory Game{" "}
+          </Heading>
+          <Spacer />
+          <Flex gap={16}>
+            <Heading>Score: {score}</Heading>
+            <Heading>Best: {best}</Heading>
+          </Flex>
+        </Flex>
+        <Grid
+          templateColumns={{
+            base: "1fr",
+            sm: "repeat(2, 1fr)",
+            xl: "repeat(4, 1fr)",
+          }}
+          gap={{ base: 6, lg: 16 }}
+        >
+          {/* <PokeCard index={"3"} handleClick={cardClick} />
                     <PokeCard index={"3"} handleClick={cardClick} />
                     <PokeCard index={"3"} handleClick={cardClick} />
                     <PokeCard index={"3"} handleClick={cardClick} />
                     <PokeCard index={"3"} handleClick={cardClick} /> */}
 
-                    {cards.map((card) => (
-                        <PokeCard
-                            key={card.id}
-                            index={card.index}
-                            handleClick={card.handleClick}
-                        />
-                    ))}
-                </Grid>
-            </Flex>
+          {cards.map((card) => (
+            <PokeCard
+              key={card.id}
+              index={card.index}
+              handleClick={card.handleClick}
+            />
+          ))}
+        </Grid>
+      </Flex>
 
-            <Modal isOpen={isOpen} onClose={onClose} isCentered>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader fontSize={32}>Game Over!</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <VStack spacing={2} align={"left"} fontSize={24}>
-                            <p>
-                                <b>Final Score: </b> {score}
-                            </p>
-                            <p>
-                                <b>Best Score: </b> {best}
-                            </p>
-                        </VStack>
-                    </ModalBody>
+      <Modal isOpen={isOpen} onClose={resetGame} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader fontSize={32}>Game Over!</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack spacing={2} align={"left"} fontSize={24}>
+              <p>
+                <b>Final Score: </b> {finalScore}
+              </p>
+              <p>
+                <b>Best Score: </b> {best}
+              </p>
+            </VStack>
+          </ModalBody>
 
-                    <ModalFooter>
-                        <Button colorScheme="teal" onClick={onClose}>
-                            Play Again
-                        </Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </Box>
-    );
+          <ModalFooter>
+            <Button colorScheme="teal" onClick={resetGame}>
+              Play Again
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Box>
+  );
 }
 
 export default App;
